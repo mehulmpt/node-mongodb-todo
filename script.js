682269f78ad8afb17a2c5036bfc75f60b873aa17
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-08-27 08:56:43 +0800

    with password secret

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const Todo = require('./models/todo')
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Todo = require("./models/todo");
 
-mongoose.connect('mongodb://localhost/firstmongo')
+mongoose.connect(
+	`mongodb+srv://andrewcbuensalida:${MONGODB_PASSWORD}@graphql-net-ninja-books.iirvr.mongodb.net/todo-mongoose-codedamn-db?retryWrites=true&w=majority`
+);
 
-app.use('/', express.static(path.resolve(__dirname, 'assets')))
+app.use("/", express.static(path.resolve(__dirname, "assets")));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/api/delete', async (req, res) => {
-	const { record } = req.body
-	console.log(record, '/api/delete')
+app.post("/api/delete", async (req, res) => {
+	const { record } = req.body;
+	console.log(record, "/api/delete");
 
-	const response = await Todo.deleteOne({ record })
+	const response = await Todo.deleteOne({ record });
 
-	console.log(response, '/api/delete repsonse')
+	console.log(response, "/api/delete repsonse");
 
-	res.json({ status: 'ok' })
-})
+	res.json({ status: "ok" });
+});
 
-app.post('/api/modify', async (req, res) => {
-	const { old: oldTitle, new: newTitle } = req.body
+app.post("/api/modify", async (req, res) => {
+	const { old: oldTitle, new: newTitle } = req.body;
 
 	const response = await Todo.updateOne(
 		{
-			record: oldTitle
+			record: oldTitle,
 		},
 		{
 			$set: {
-				record: newTitle
-			}
+				record: newTitle,
+			},
 		}
-	)
+	);
 
-	console.log(response)
+	console.log(response);
 
-	res.json({ status: 'ok' })
-})
+	res.json({ status: "ok" });
+});
 
-app.get('/api/get', async (req, res) => {
-	const records = await Todo.find({})
+app.get("/api/get", async (req, res) => {
+	const records = await Todo.find({});
 	// console.log('Response => ', records)
-	res.json(records)
-})
+	res.json(records);
+});
 
-app.post('/api/create', async (req, res) => {
-	const record = req.body
-	console.log(record)
+app.post("/api/create", async (req, res) => {
+	const record = req.body;
+	console.log(record);
 
 	// * CREATE (_C_RUD)
-	const response = await Todo.create(record)
+	const response = await Todo.create(record);
 
-	console.log(response)
+	console.log(response);
 
-	res.json({ status: 'ok' })
-})
+	res.json({ status: "ok" });
+});
 
-app.listen(13371, '127.0.0.1', () => {
-	console.log('Server up')
-})
+app.listen(13371, "127.0.0.1", () => {
+	console.log("Server up");
+});
