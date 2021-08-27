eaacee496ed0eadcc092d3e06993c9d62b1dd0a9
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-08-27 19:56:16 +0800

    port 4000

--- script.js
+++ script.js
@@ -61,6 +61,6 @@ app.post("/api/create", async (req, res) => {
 	res.json({ status: "ok" });
 });
 
-app.listen(13371, "127.0.0.1", () => {
+app.listen(4000, () => {
 	console.log("Server up");
 });
